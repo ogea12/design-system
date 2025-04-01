a760df8f5b61cb78894d69a60e4e1d64f4ea0799
Julien Benac <julien.benac@protonmail.com>
2025-04-01 15:28:18 +0200

    ci: resolve the lint check failures

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,10 +6,23 @@ import {
   transformerVariantGroup,
 } from 'unocss'
 
-import { colors, fontSize } from './src/tokens'
+import { colors } from './src/tokens'
 
 export default defineConfig({
-  theme: { colors, fontSize },
+  theme: {
+    colors,
+    fontSize: {
+      '2xs': ['0.75rem', '100%'],
+      'xs': ['0.8125rem', '120%'],
+      'sm': ['0.875rem', '140%'],
+      'base': ['1rem', '140%'],
+      'lg': ['1.125rem', '140%'],
+      'xl': ['1.375rem', '120%'],
+      '2xl': ['1.5rem', '120%'],
+      '3xl': ['1.75rem', '110%'],
+      '4xl': ['2rem', '100%'],
+    },
+  },
   content: { filesystem: ['./src/**/*.tsx'] },
   presets: [
     presetWind3({ dark: 'class' }),
